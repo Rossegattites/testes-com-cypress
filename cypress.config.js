94adce967e99523d5c104be53c5f6bef88cd54ab
Rossegattites <rossegattites15@gmail.com>
2023-06-17 17:15:55 -0300

    segundo commit

--- cypress.config.js
+++ cypress.config.js
@@ -6,9 +6,10 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     excludeSpecPattern: ['**/cypress/e2e/1-getting-started','**/cypress/e2e/2-advanced-examples'],
-   baseUrl: 'https://www.saucedemo.com/'
+   baseUrl: "https://www.saucedemo.com/",
+   "chromeWebSecurity": false
   },
-  
-   
 
 });
+
+
